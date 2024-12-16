f7b903b7a05ca4af2c7e423b5d4902da98e057fb
vethumIIT <vethum.20231974@iit.ac.lk>
2024-12-16 18:41:22 +0530

    Implemented user login and registration in backend
    Implemented user login and registration in the backend with database entries, password encryption, status codes for different errors and database interactions.

--- BACKEND/Fably-Fashion-App/src/main/java/com/project/Fably_Fashion_App/services/UserService.java
+++ BACKEND/Fably-Fashion-App/src/main/java/com/project/Fably_Fashion_App/services/UserService.java
@@ -3,8 +3,11 @@ package com.project.Fably_Fashion_App.services;
 
 import com.project.Fably_Fashion_App.models.User;
 import com.project.Fably_Fashion_App.repositories.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -13,14 +16,42 @@ public class UserService {
     private UserRepository userRepository;
 
 
-    private String login(User user) {
-        return "login service";
+    public String login(User user, HttpSession session) {
+        Map<String, Object> map = userRepository.getUserByEmail(user.getEmail());
+        if (map==null){
+            return "Not Exist";
+        }
+        boolean correctPassword = PasswordHasher.checkPassword(user.getPassword(), map.get("password").toString());
+        if (correctPassword) {
+            session.setAttribute("user", user);
+            return "Valid";
+        }else{
+            return "Invalid";
+        }
     }
 
-    private String register(User user) {
+    public String register(User user) {
         user.setPassword(PasswordHasher.hash(user.getPassword()));
-        // Set password to the encrypted version.
-        return "register service";
+        if(userRepository.getUserByEmail(user.getEmail())!=null){
+            return "User exists";
+        }else {
+            userRepository.createUser(user);
+            return "User Registered";
+        }
+    }
+
+    public String deleteUser(User user, HttpSession session) {
+        Map<String, Object> map = userRepository.getUserByEmail(user.getEmail());
+        if (!PasswordHasher.checkPassword(user.getPassword(), map.get("password").toString())) {
+            return "Invalid Password";
+        }
+        int success = userRepository.deleteUser(user.getEmail());
+        if(success>=1){
+            session.invalidate();
+            return "User Deleted";
+        }else {
+            return "Failed";
+        }
     }
 
 }
