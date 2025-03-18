66e93134951f82000bcb8a824c9e0f923c1e3b72
Vethum Buthgamuwa <160003991+vethumIIT@users.noreply.github.com>
2025-03-18 11:51:47 +0530

    Made items in viton history tapable
    * When a in progress item in the try on history page is tapped, it checks the status of the image generation

--- FRONTEND/fably/lib/screens/scanner/vton_history.dart
+++ FRONTEND/fably/lib/screens/scanner/vton_history.dart
@@ -27,6 +27,33 @@ class _VtonHistoryPageState extends State<VtonHistoryPage> {
         .showSnackBar(SnackBar(content: Text(message)));
   }
 
+  Future<void> getVtonStatus(index) async {
+    final requests = BackendRequests();
+
+    try {
+      final response = await requests.postRequest("vton/fetch_url/", body: {
+        "vton_id": vtonHistory[index]['vtonId'],
+        "item_id": vtonHistory[index]['itemId'],
+      });
+
+      if (response.statusCode == 200) {
+        if (response.body == "processing") {
+          _showMessage("Still processing image");
+        } else {
+          setState(() {
+            vtonHistory[index]['status'] = "completed";
+          });
+          _showMessage("Your try on is ready");
+        }
+        //_showMessage('POST request successful: ${response.body}');
+      } else {
+        print('Failed to send POST request: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error occurred while sending POST request: $error');
+    }
+  }
+
   void fetchVtonHistory() async {
     // Simulating an API call or database fetch
     final requests = BackendRequests();
@@ -48,77 +75,109 @@ class _VtonHistoryPageState extends State<VtonHistoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("VTON History"),
-        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop(); // Navigate back to the previous screen
+          },
+        ),
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Try-On History",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true, // Centers the title
       ),
       body: vtonHistory.isEmpty
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
               itemCount: vtonHistory.length,
               itemBuilder: (context, index) {
-                //final item = vtonHistory.entries.toList()[index];
                 final item = vtonHistory[index];
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  elevation: 4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child:item["status"] == "processing"
-                          ? SizedBox(
-                              height: 180, // Match the image height for alignment
-                              width: double.infinity,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-                                crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-                                children: [
-                                  Icon(
-                                    Icons.hourglass_empty,
-                                    size: 50,
-                                    color: Colors.grey,
-                                  ),
-                                  const SizedBox(height: 8),
-                                  const Text(
-                                    "Processing",
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey,
+                return InkWell(
+                  onTap: () {
+                    // if the image is still processing
+                    if (vtonHistory[index]['status'] == 'processing') {
+                      getVtonStatus(index);
+                    } else {
+                      String itemName = vtonHistory[index]['name'];
+                      String personImageUrl = vtonHistory[index]['personPhoto'];
+                      String clothPhoto = vtonHistory[index]['clothPhoto'];
+                      String vtonPhoto = vtonHistory[index]['imageUrl'];
+
+                      // Navigator.push here.
+
+                    }
+                  },
+                  borderRadius: BorderRadius.circular(
+                      10), // Ensure ripple effect matches card shape
+                  child: Card(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 4,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: item["status"] == "processing"
+                                ? SizedBox(
+                                    height:
+                                        180, // Match the image height for alignment
+                                    width: double.infinity,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .center, // Center vertically
+                                      crossAxisAlignment: CrossAxisAlignment
+                                          .center, // Center horizontally
+                                      children: [
+                                        Icon(
+                                          Icons.hourglass_empty,
+                                          size: 50,
+                                          color: Colors.grey,
+                                        ),
+                                        const SizedBox(height: 8),
+                                        const Text(
+                                          "Processing",
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                : AspectRatio(
+                                    aspectRatio: 3 / 4,
+                                    child: Image.network(
+                                      item["imageUrl"]!,
+                                      height: 180,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                      errorBuilder:
+                                          (context, error, stackTrace) {
+                                        return const Icon(Icons.error,
+                                            size: 50);
+                                      },
                                     ),
                                   ),
-                                ],
-                              ),
-                            )
-                          : AspectRatio(
-                                aspectRatio: 3 / 4,
-                                child: Image.network(
-                                item["imageUrl"]!,
-                                height: 180,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Icon(Icons.error, size: 50);
-                                },
-                              ),
-                            ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          item["name"] ?? "Unknown Item",
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 10),
+                          Text(
+                            item["name"] ?? "Unknown Item",
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
